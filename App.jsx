68e9bef1083081e9fdfdff9e98c147d37659a88e
Ruan <130461902+ruantasca@users.noreply.github.com>
2024-04-16 11:58:45 -0300

    flexboat

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import Flexbox from './components/Flexbox'
+import Botao from './components/Botao'
+import './App.css'
+import './Style.css'
+
+
+function App() {
+
+  return (
+    <main>
+    <Flexbox />
+    <Botao />
+    </main>
+  )
+}
+
+export default App
